ae2eb6628b466d27d8b144cb0354e203b00cfcbd
Nishka Trivedi <trivedinc@icloud.com>
2026-03-05 11:58:02 -0500

    implemented key control

--- Zelda/Main/GamePanel.java
+++ Zelda/Main/GamePanel.java
@@ -2,6 +2,8 @@ package Zelda.Main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
@@ -17,12 +19,23 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    // FPS
+
+    
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+
+    // Set players default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel (){
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread(){
@@ -34,8 +47,41 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        while(gameThread != null) {
+            long currentTime = System.nanoTime();
+            System.out.println("current time: "+currentTime);
+
+            // 1. UPDATE: update information such as character positions
+            update();
+
+            // 2. DRAW: draw the screen with the updated information
+            repaint();
+
+        }
         
-        
+    }
+    public void update(){
+        if(keyH.upPressed == true){
+            playerY -= playerSpeed;
+        }
+        else if(keyH.downPressed == true){
+            playerY += playerSpeed;
+        }
+
+        if(keyH.leftPressed == true){
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed == true){
+            playerX += playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setColor(Color.WHITE);
+        g2.fillRect(playerX,playerY,tileSize, tileSize);
+        g2.dispose();
     }
     
 }
--- Zelda/Main/Main.java
+++ Zelda/Main/Main.java
@@ -9,7 +9,15 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("Zelda");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 }
