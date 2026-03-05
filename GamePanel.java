a036df6093885c1f0d73138903935969b5f2e276
neeshkee <nctriv28@students.shrewsbury.k12.ma.us>
2026-03-05 11:04:37 -0500

    initial upload

---
+++ GamePanel.java
@@ -0,0 +1,17 @@
+package Zelda.Main;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+    
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package Zelda.Main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    
+    public static void main(String [] args){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Zelda");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
