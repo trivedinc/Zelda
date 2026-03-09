b97aa5e587f5c4ef43cf8761d0feeccab3ce74c9
Nishka Trivedi <trivedinc@icloud.com>
2026-03-09 09:32:30 -0400

    3/9

--- Zelda/Main/GamePanel.java
+++ Zelda/Main/GamePanel.java
@@ -7,23 +7,26 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
+import Zelda.Main.entity.Player;
+
 public class GamePanel extends JPanel implements Runnable{
     
     // SCREEN SETTINGS
     final int originalTileSize = 16; //16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
     // FPS
-
+    int FPS = 60;
     
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player=new Player(this,keyH);
 
     // Set players default position
     int playerX = 100;
@@ -47,9 +50,11 @@ public class GamePanel extends JPanel implements Runnable{
 
     @Override
     public void run() {
+        double drawInterval = 1000000000/FPS; //0.0166666 seconds
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
         while(gameThread != null) {
-            long currentTime = System.nanoTime();
-            System.out.println("current time: "+currentTime);
+            
 
             // 1. UPDATE: update information such as character positions
             update();
@@ -57,30 +62,35 @@ public class GamePanel extends JPanel implements Runnable{
             // 2. DRAW: draw the screen with the updated information
             repaint();
 
+            
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+
+                remainingTime=remainingTime/1000000;
+
+                if (remainingTime<0) {
+                    remainingTime=0;
+                }
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime+=drawInterval;
+
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
         }
         
     }
     public void update(){
-        if(keyH.upPressed == true){
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }
-
-        if(keyH.leftPressed == true){
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.WHITE);
-        g2.fillRect(playerX,playerY,tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
     }
     
---
+++ Zelda/Main/entity/Entity.java
@@ -0,0 +1,18 @@
+package Zelda.Main.entity;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+
+public class Entity {
+    public int x,y;
+    public int speed;
+
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+
+}
